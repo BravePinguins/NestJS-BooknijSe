3bd00e13392fa89de6df05e1e24c30cad99bd241
Brylson123 <csowscy@gmail.com>
2024-03-23 16:16:22 +0100

    feat: Change name of interface

--- src/models/property-type.model.ts
+++ src/models/property-type.model.ts
@@ -2,15 +2,12 @@ import { Column, DataType, HasMany, Model, Table } from "sequelize-typescript";
 import { ApiProperty } from "@nestjs/swagger";
 import { Property } from "./property.model";
 
-interface CreatePropertyAmenityAttr {
+interface CreatePropertyTypeAttr {
   value: string;
 }
 
 @Table({ tableName: "property_type", createdAt: false, updatedAt: false })
-export class PropertyType extends Model<
-  PropertyType,
-  CreatePropertyAmenityAttr
-> {
+export class PropertyType extends Model<PropertyType, CreatePropertyTypeAttr> {
   @ApiProperty({
     example: "4714b63e-d984-11eb-b8bc-0242ac130003",
     description: "Unique Id",
