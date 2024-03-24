61d04feca66fd76ebb4a9f9b4b10413842234f56
Tommy Phan <tommyphan2004.tp@gmail.com>
2024-03-24 15:19:02 +0100

    feat: add controller for property types

---
+++ src/controllers/property-type.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { PropertyType } from "src/models/property-type.model";
+import { PropertyTypeService } from "src/services/property-type.service";
+
+@ApiTags("Property type")
+@Controller("property-type")
+export class PropertyTypeController {
+  constructor(private propertyTypeService: PropertyTypeService) {}
+
+  @Get("/")
+  @ApiOperation({ summary: "Get all property types" })
+  @ApiResponse({ status: 200, type: [PropertyType] })
+  getAllPropertyTypes() {
+    return this.propertyTypeService.getAllPropertyTypes();
+  }
+}
