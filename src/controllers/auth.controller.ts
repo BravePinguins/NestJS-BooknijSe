9695d9707566a5c0c255ef6891b9bc424e059126
Bryła <csowscy@gmail.com>
2024-04-09 23:24:18 +0200

    feat: Add endpoint description to swagger

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,23 +4,23 @@ import { AuthLoginDto } from "../models/dto/auth/auth-login.dto";
 import { User } from "../models/user.model";
 import { UserObj } from "../common/decorators/user-obj.decorator";
 import { Response } from "express";
-import { UserService } from "../services/user.service";
+import { ApiBody, ApiOperation } from "@nestjs/swagger";
 @Controller("auth")
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private userService: UserService
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
-  @Get("/getUsers")
-  async getUser() {
-    return this.userService.getUser();
-  }
   @Post("/login")
+  @ApiOperation({
+    summary: "User Login",
+  })
+  @ApiBody({ type: AuthLoginDto })
   async login(@Body() req: AuthLoginDto, @Res() res: Response): Promise<any> {
     return this.authService.login(req, res);
   }
   @Get("/logout")
+  @ApiOperation({
+    summary: "User logout",
+  })
   async logout(@UserObj() user: User, @Res() res: Response) {
     return this.authService.logout(user, res);
   }
