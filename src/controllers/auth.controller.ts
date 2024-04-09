b797f5413edbdc21022cf24f45334be4bfe80880
Bryła <csowscy@gmail.com>
2024-04-10 00:33:15 +0200

    feat: Add swagger

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,18 +4,24 @@ import { AuthLoginDto } from "../models/dto/auth/auth-login.dto";
 import { User } from "../models/user.model";
 import { UserObj } from "../common/decorators/user-obj.decorator";
 import { Response } from "express";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiOperation, ApiTags } from "@nestjs/swagger";
 @ApiTags("Auth")
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("/login")
-  async login(@Body() dto: AuthLoginDto, @Res() res: Response): Promise<any> {
-    return this.authService.login(dto, res);
+  @ApiOperation({
+    summary: "User Login",
+  })
+  @ApiBody({ type: AuthLoginDto })
+  async login(@Body() req: AuthLoginDto, @Res() res: Response): Promise<any> {
+    return this.authService.login(req, res);
   }
-
   @Get("/logout")
+  @ApiOperation({
+    summary: "User logout",
+  })
   async logout(@UserObj() user: User, @Res() res: Response) {
     return this.authService.logout(user, res);
   }
--- src/models/dto/auth/auth-login.dto.ts
+++ src/models/dto/auth/auth-login.dto.ts
@@ -1,10 +1,19 @@
 import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class AuthLoginDto {
+  @ApiProperty({
+    example: "john@example.com",
+    description: "User email address",
+  })
   @IsString()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: "password123",
+    description: "User password",
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
